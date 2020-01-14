2765f09f7ed8abc588e1d307c6dacdeb59397965
nenad <nenad.syncit@gmail.com>
2020-01-14 13:06:46 +0100

    get all attributes ids  and names

---
+++ Block/Attributes.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * SyncIt Group
+ *
+ * This source file is subject to the SyncIt Software License, which is available at https://syncitgroup.com/.
+ * Do not edit or add to this file if you wish to upgrade the to newer versions in the future.
+ * If you wish to customize this module for your needs.
+ * Please refer to http://www.magentocommerce.com for more information.
+ *
+ * @category  SyncIt
+ * @package   SyncIt_PowerTools
+ * @author    Nenad Mihajlović <nenad@syncitgroup.com>
+ * @link      https://syncitgroup.com/
+ * @license   http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @copyright Copyright (C) 2019 SyncIt (https://syncitgroup.com/)
+ */
+namespace Syncit\PowerTools\Block;
+
+/**
+ *  Get all atrtributes ids and names
+ */
+class Attributes extends \Magento\Framework\View\Element\Template
+{
+
+    protected $_attributeFactory;
+
+    public function __construct(
+      
+       \Magento\Catalog\Model\ResourceModel\Eav\Attribute $attributeFactory
+      
+   ) {
+       
+       $this->_attributeFactory = $attributeFactory;
+       
+   }
+   
+   public function getAllAttributesIds()
+   {
+       $attributeInfo = $this->_attributeFactory->getCollection();
+   
+      foreach($attributeInfo as $attributes)
+      {
+           $attributeId = $attributes->getAttributeId();
+           // You can get all fields of attribute here
+           var_dump($attributeId );
+      }
+   }
+   public function getAllAttributesNames()
+   {
+       $attributeInfo = $this->_attributeFactory->getCollection();
+   
+      foreach($attributeInfo as $attributes)
+      {
+           $attributeName = $attributes->getFrontendLabel();
+           // You can get all fields of attribute here
+           var_dump($attributeName );
+      }
+   }
+
+}
